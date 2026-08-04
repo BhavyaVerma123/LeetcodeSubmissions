fd54be71c57ee874cdfeb62d3fc7f684a140f107
Bhavya Verma <79978358+BhavyaVerma123@users.noreply.github.com>
2026-08-04 22:44:12 +0530

    Time: 282 ms (53.08%) | Memory: 29 MB (16.20%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -12,12 +12,12 @@ class Solution(object):
 
             if q and q[0] == i - k:
                 q.popleft()
-
+            
             while q and nums[q[-1]] <= nums[i]:
                 q.pop()
+
             q.append(i)
-            
+
             if i >= k-1:
                 res.append(nums[q[0]])
-                
         return res
